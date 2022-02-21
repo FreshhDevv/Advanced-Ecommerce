8cc8abde06a5dd387a4a18fea07357a27a1effd5
FreshDev <billyhans90@gmail.com>
2022-02-21 10:03:54 +0100

    Admin Profile & Image update part 3

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
 <div class="container-full">
@@ -47,15 +48,15 @@
                             <div class="form-group">
                                 <h5>Profile Image<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="file" name="profile_photo_path" class="form-control" required="">
-                                    <div class="help-block"></div>
+                                    <input type="file" name="profile_photo_path" class="form-control" required="" id="image">
+                                    
                                 </div>
                             </div>
                             </div>
 
                             <div class="col-md-6">
 
-                            <img src="{{ (!empty($adminData->profile_photo_path))?
+                            <img id="showImage" src="{{ (!empty($adminData->profile_photo_path))?
                          url('upload/admin_images/'.$adminData->profile_photo_path):url('upload/no_image.jpg') }}" style="width:100px; height:100px;">
 
                             </div>
@@ -80,6 +81,17 @@
 
 </div>
 
+<script type="text/javascript">
+$(document).ready(function(){
+    $('#image').change(function(e){
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#showImage').attr('src',e.target.result);
+        }
+        reader.readAsDataURL(e.target.files['0']);
+    });
+});
+</script>
 
 
 @endsection
