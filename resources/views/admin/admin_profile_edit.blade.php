3bf513be71b31482d807c1361fe7a3a8e4875725
FreshDev <billyhans90@gmail.com>
2022-02-21 09:24:11 +0100

    Turns out, when your date and time is not correct, it messes with my machine and the way my code is rendered

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -5,66 +5,66 @@
 <div class="container-full">
 
 
-    <section class="content">
+<section class="content">
 
-        <!-- Basic Forms -->
-        <div class="box">
-            <div class="box-header with-border">
-                <h4 class="box-title">Admin Profile Edit</h4>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <div class="row">
-                    <div class="col">
-                        <form novalidate="">
-                            <div class="row">
-                                <div class="col-12">
+<!-- Basic Forms -->
+<div class="box">
+    <div class="box-header with-border">
+        <h4 class="box-title">Admin Profile Edit</h4>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+        <div class="row">
+            <div class="col">
+                <form novalidate="">
+                    <div class="row">
+                        <div class="col-12">
 
-                                <div class="row">
-                                    <div class="col-md-6">
-                                    <div class="form-group">
-                                        <h5>Admin User Name <span class="text-danger">*</span></h5>
-                                        <div class="controls">
-                                            <input type="text" name="name" class="form-control" required="">     
-                                        </div>
-                                    </div>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                    <div class="form-group">
-                                        <h5>Admin Email <span class="text-danger">*</span></h5>
-                                        <div class="controls">
-                                            <input type="email" name="email" class="form-control" required="">     
-                                        </div>
-                                    </div>
-                                    </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                            <div class="form-group">
+                                <h5>Admin User Name <span class="text-danger">*</span></h5>
+                                <div class="controls">
+                                    <input type="text" name="name" class="form-control" required="">     
                                 </div>
-               
-                                    
-                                    
-                                    <div class="form-group">
-                                        <h5>Fil Input Field <span class="text-danger">*</span></h5>
-                                        <div class="controls">
-                                            <input type="file" name="file" class="form-control" required="">
-                                            <div class="help-block"></div>
-                                        </div>
-                                    </div>
-                                   
-                            <div class="text-xs-right">
-                                <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update">
                             </div>
-                        </form>
+                            </div>
 
+                            <div class="col-md-6">
+                            <div class="form-group">
+                                <h5>Admin Email <span class="text-danger">*</span></h5>
+                                <div class="controls">
+                                    <input type="email" name="email" class="form-control" required="">     
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+       
+                            
+                            
+                            <div class="form-group">
+                                <h5>File Input Field <span class="text-danger">*</span></h5>
+                                <div class="controls">
+                                    <input type="file" name="file" class="form-control" required="">
+                                    <div class="help-block"></div>
+                                </div>
+                            </div>
+                           
+                    <div class="text-xs-right">
+                        <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update">
                     </div>
-                    <!-- /.col -->
-                </div>
-                <!-- /.row -->
-            </div>
-            <!-- /.box-body -->
-        </div>
-        <!-- /.box -->
+                </form>
 
-    </section>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.box-body -->
+</div>
+<!-- /.box -->
+
+</section>
 
 </div>
 
