6bba34a387ccb409fed6242fc68d293cd45998a7
FreshDev <billyhans90@gmail.com>
2022-04-29 13:08:07 +0100

    Add the add brand page

--- resources/views/backend/brand/brand_view.blade.php
+++ resources/views/backend/brand/brand_view.blade.php
@@ -11,7 +11,7 @@
             <div class="row">
 
 
-                <div class="col-12">
+                <div class="col-8">
 
                     <div class="box">
                         <div class="box-header with-border">
@@ -53,6 +53,51 @@
                    
                 </div>
                 <!-- /.col -->
+
+                <!-- -------------Add Brand Page-------------- -->
+
+                <div class="col-4">
+
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Add Brand</h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <div class="table-responsive">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Brand En</th>
+                                            <th>Brand Fre</th>
+                                            <th>Image</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($brands as $item)
+                                        <tr>
+                                            <td>{{ $item->brand_name_en }}</td>
+                                            <td>{{ $item->brand_name_fre }}</td>
+                                            <td><img src="{{ asset($item->brand_image) }}" alt="" style="width: 70px; height: 40px;"></td>
+                                            <td>
+                                                <a href="http://" class="btn btn-info">Edit</a>
+                                                <a href="http://" class="btn btn-danger">Delete</a>
+                                            </td>
+                                            
+                                        </tr>
+                                        @endforeach
+                                       
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+
+                   
+                </div>
+
             </div>
             <!-- /.row -->
         </section>
