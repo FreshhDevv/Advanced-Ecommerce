11369952e7b3637c4d718967e30572556e83b626
FreshDev <billyhans90@gmail.com>
2022-05-05 17:59:50 +0100

    Refactoring

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -12,32 +12,34 @@ class BrandController extends Controller
     public function BrandView()
     {
         $brands = Brand::latest()->get();
-        return view('backend.brand.brand_view',compact('brands'));
+        return view('backend.brand.brand_view', compact('brands'));
     }
 
     public function BrandStore(Request $request)
     {
-        $request->validate([
-            'brand_name_en' => 'required',
-            'brand_name_fre' => 'required',
-            'brand_image' => 'required',
-        ],
-        [
-            'brand_name_en.required' => 'The Brand Name English field is required',
-            'brand_name_fre.required' => 'The Brand Name French field is required',
-        ]);
+        $request->validate(
+            [
+                'brand_name_en' => 'required',
+                'brand_name_fre' => 'required',
+                'brand_image' => 'required',
+            ],
+            [
+                'brand_name_en.required' => 'The Brand Name English field is required',
+                'brand_name_fre.required' => 'The Brand Name French field is required',
+            ]
+        );
 
         $image = $request->file('brand_image');
-        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-        Image::make($image)->resize(300,300)->save('upload/brands/'.$name_gen);
-        $save_url = 'upload/brands/'.$name_gen;
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(300, 300)->save('upload/brands/' . $name_gen);
+        $save_url = 'upload/brands/' . $name_gen;
 
         Brand::insert([
             'brand_name_en' => $request->brand_name_en,
             'brand_name_fre' => $request->brand_name_fre,
-            'brand_slug_en' => strtolower(str_replace(' ','-',$request->brand_name_en)),
-            'brand_slug_fre' => str_replace(' ','-',$request->brand_name_fre),
-            'brand_image' =>$save_url,
+            'brand_slug_en' => strtolower(str_replace(' ', '-', $request->brand_name_en)),
+            'brand_slug_fre' => str_replace(' ', '-', $request->brand_name_fre),
+            'brand_image' => $save_url,
         ]);
 
         $notification = array(
@@ -45,7 +47,5 @@ class BrandController extends Controller
             'alert-type' => 'success'
         );
         return redirect()->back()->with($notification);
-
-
     }   //End Method
 }
