cc6482639c06c120d6bfbfe0cd4b4a11b649056e
FreshDev <billyhans90@gmail.com>
2022-05-07 15:01:18 +0100

    Update brand method

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -53,13 +53,30 @@ class BrandController extends Controller
         return view('backend.brand.brand_edit', compact('brand'));
     }
 
-    public function BrandUpdate(Request $request) 
+    public function BrandUpdate(Request $request)
     {
         $brand_id = $request->id;
         $old_img = $request->old_image;
 
-        if($request->file('brand_image')) {
-            
+        if ($request->file('brand_image')) {
+            unlink($old_img);
+            $image = $request->file('brand_image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(300, 300)->save('upload/brands/' . $name_gen);
+            $save_url = 'upload/brands/' . $name_gen;
+
+            Brand::insert([
+                'brand_name_en' => $request->brand_name_en,
+                'brand_name_fre' => $request->brand_name_fre,
+                'brand_slug_en' => strtolower(str_replace(' ', '-', $request->brand_name_en)),
+                'brand_slug_fre' => str_replace(' ', '-', $request->brand_name_fre),
+                'brand_image' => $save_url,
+            ]);
+            $notification = array(
+                'message' => 'Brand Added Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->back()->with($notification);
         }
     }   //End Method
 }
